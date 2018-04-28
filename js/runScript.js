eacf529ae2b6ea24788c0dbbbc6d2d30ebb275a3
is23 <37001229+is23@users.noreply.github.com>
2018-04-27 23:52:32 -0400

    added warning system
     to add red or blue code classes it give alert

--- js/runScript.js
+++ js/runScript.js
@@ -13,119 +13,117 @@ function setUser() { //get classes for user
     di["ucid"] = n;
     di["major"] = "Science1";
     var req = {"x": di};
-    $.post('../getAllClassesStudent.php',req,(data, textStatus, jqXHR) => {console.log(data);loadClasses(data); loadSemesters(data);},"text");
+    $.post('../getAllClassesStudent.php',req,(data, textStatus, jqXHR) => {
+        console.log(data);
+        loadClasses(data);
+        loadSemesters(data);
+    },"text");
+}
+
+function findClassStatus(data) {
+    var result = JSON.parse(data);
+    var tempGroupArr =[];
+    var final=[];
+    var refined={};
+    var classes = [];
+    var inDict = {};
+    var classDetail = [];
+    var prer = [];
+    var check = false;
+    var index;
+    for (x in result) {
+        if(!(tempGroupArr.includes(result[x].group))) {
+            tempGroupArr.push(result[x].group);
+            final.push({"group":result[x].group});
+        }
+    }
+    txt="";
+    for (x in final) {
+        final[x]["classes"]=[];
+        for(i in result) {
+            if(final[x]["group"] == result[i]["group"]){
+                classDetail = [];
+                inDict = {};
+                inDict = {"class":"","detail":classDetail};
+                index = result[i].order-1;
+                inDict["class"]=result[i].class;
+                inDict["code"] = result[i].code;
+                if(result[i].prereqs.length == 0) {
+                    check = true;
+                }
+                else {
+                    for(y in result[i].prereqs){
+
+                       prer.push(result[i].prereqs[y].prereq);
+                       if(result[i].prereqs[y].logic == 'OR' && result[i].prereqs[y].code != null){
+                           check = true;
+                           break;
+                       }
+                       else if (result[i].prereqs[y].logic == 'OR' && result[i].prereqs[y].code == null){
+                           check = false;
+                       }
+                       else if (result[i].prereqs[y].logic == 'AND' && result[i].prereqs[y].code != null){
+                           check = true;
+                       }
+                       else if (result[i].prereqs[y].logic == 'AND' && result[i].prereqs[y].code == null){
+                           check = false;
+                       }
+                    }
+                }
+                if(check == true) {
+                    inDict["available"] = 0;
+                }
+                else {
+                    inDict["available"] = 1;
+                }
+                //this make the prereq array and then empty it for next class
+                if(prer.length == 0){
+                    prer.push("No Prereq");
+                }
+                inDict["pre"]=prer;
+                prer=[];
+
+                classDetail.push(result[i].description);
+                inDict["detail"]=classDetail;
+                final[x]["classes"].push(inDict);
+            }
+        }
+    }
+    return final;
 }
 
 function loadClasses(data) {// load classses with color code
-            var result = JSON.parse(data);
-            var tempGroupArr =[];
-            var final=[];
-            var refined={};
-            var classes = [];
-            var inDict = {};
-            var classDetail = [];
-            var prer = [];
-            var check = false;
-            var index;
-            for (x in result) {
-                if(!(tempGroupArr.includes(result[x].group))) {
-                    tempGroupArr.push(result[x].group);
-                    final.push({"group":result[x].group});
-                }
-            }
-            txt="";
-
-            for (x in final) {
-                final[x]["classes"]=[];
-                for(i in result) {
-                    if(final[x]["group"] == result[i]["group"]){
-                        classDetail = [];
-
-                        inDict = {};
-                        inDict = {"class":"","detail":classDetail};
-                        index = result[i].order-1;
-                        inDict["class"]=result[i].class;
-                        inDict["code"] = result[i].code;
-                        if(result[i].prereqs.length == 0) {
-                            check = true;
-                        }
-                        else {
-                            for(y in result[i].prereqs){
-
-                               prer.push(result[i].prereqs[y].prereq);
-                               if(result[i].prereqs[y].logic == 'OR' && result[i].prereqs[y].code != null){
-                                   check = true;
-                                   break;
-                               }
-                               else if (result[i].prereqs[y].logic == 'OR' && result[i].prereqs[y].code == null){
-                                   check = false;
-                               }
-                               else if (result[i].prereqs[y].logic == 'AND' && result[i].prereqs[y].code != null){
-                                   check = true;
-                               }
-                               else if (result[i].prereqs[y].logic == 'AND' && result[i].prereqs[y].code == null){
-                                   check = false;
-                               }
-                              // if(result[i].prereqs[y].logic == null && result[i].prereqs[y].code != null) {
-                                //   check = true;
-                               //}
-                               //else {
-                                 //  check = false;
-                               //}
-                            }
-                        }
-                        if(check == true) {
-                            inDict["available"] = 0;
-                        }
-                        else {
-                            inDict["available"] = 1;
-                        }
-                        //this make the prereq array and then empty it for next class
-                        if(prer.length == 0){
-                            prer.push("No Prereq");
-                        }
-                        inDict["pre"]=prer;
-                        prer=[];
-
-                        classDetail.push(result[i].description);
-                        inDict["detail"]=classDetail;
-                        final[x]["classes"].push(inDict);
-
+    let final = findClassStatus(data)
+    for(x in final) {
+        txt += "<li class=\"gurList\">" +
+                "<a class=\"is-active\">" + final[x].group + "</a>" +
+                "<ul>";
+                for(i in final[x].classes) {
+                    if(final[x].classes[i].code != null){
+                        txt+= "<li class=\"blueClass h\" >" +
+                                "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
+                                "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
+                            "</li>";
                     }
-
-
-                }
-            }
-            for(x in final) {
-                txt += "<li class=\"gurList\">" +
-                        "<a class=\"is-active\">" + final[x].group + "</a>" +
-                        "<ul>";
-                        for(i in final[x].classes) {
-                            if(final[x].classes[i].code != null){
-                                txt+= "<li class=\"blueClass h\" >" +
-                                        "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
-                                        "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
-                                    "</li>";
-                            }
-                            else {
-                                if(final[x].classes[i].available == 0){
-                                    txt+= "<li class=\"greenClass h\" >" +
-                                        "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
-                                        "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
-                                    "</li>";
-                                }
-                                else {
-                                    txt+= "<li class=\"redClass h\" >" +
-                                        "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
-                                        "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
-                                    "</li>";
-                                }
-                            }
+                    else {
+                        if(final[x].classes[i].available == 0){
+                            txt+= "<li class=\"greenClass h\" >" +
+                                "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
+                                "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
+                            "</li>";
                         }
-                        txt+= "</ul>" +
-                    "</li>";
-            }
-            document.getElementById("classesMenu").innerHTML = txt;
+                        else {
+                            txt+= "<li class=\"redClass h\" >" +
+                                "<a>" + final[x].classes[i].class + " (" + final[x].classes[i].detail +")" +
+                                "<span class=\"hoverText\">"+ final[x].classes[i].pre+"</span>" + "</a>"+
+                            "</li>";
+                        }
+                    }
+                }
+                txt+= "</ul>" +
+            "</li>";
+    }
+    document.getElementById("classesMenu").innerHTML = txt;
 }
 
 
@@ -293,41 +291,101 @@ function sortYear(a, b) { // for sorting semesters
 }
 
 function sumbmitSemester() {
+    var querys = window.location.search.substring(1);
+    var n = querys.split("=").pop();
+    var di ={};
+    di["ucid"] = n;
+    di["major"] = "Science1";
+    var req = {"x": di};
+    $.post('../getAllClassesStudent.php',req,(data, textStatus, jqXHR) => {
+        var x = document.getElementById("createSem");
+        x.style.display = "none";
+        var semesterName = document.getElementById("year").value;
+        var clss = [];
+        var arr = document.getElementsByClassName('eachClass');
+        for(i=0 ; i < arr.length ; i++) {
+            clss.push(arr[i].value);
+        }
 
-  var x = document.getElementById("createSem");
-    x.style.display = "none";
-    var semesterName = document.getElementById("year").value;
-    var classes = [];
-    var arr = document.getElementsByClassName('eachClass');
-    for(i=0 ; i < arr.length ; i++) {
-        classes.push(arr[i].value);
-    }
-    var code ="";
-    code+=semesterName;
-    if( document.getElementsByClassName('sem')[0].checked == true) {
-        code+="S";
-    }
-    else {
-        code+="F";
-    }
-    var final=[];
-    var di ={}
-        //get username
-        var querys = window.location.search.substring(1);
-        var n = querys.split("=").pop();
-    for(j in classes) {
-        di["name"]=classes[j];
-        di["ucid"]=n;
-        di["code"]=code;
-        di["major"]="Science1";
-        final.push(di);
-        di={};
-    }
+        let final = findClassStatus(data);
+        let alertSet = [];
+        let tempDict = {};
+        let notAllowed = false;
+        for(x in final) {
+            for(i in final[x].classes) {
+                if(final[x].classes[i].code != null && clss.indexOf(final[x].classes[i].class) > -1){
+                    tempDict["class"] = final[x].classes[i].class;
+                    tempDict["prereq"] = "taken";
+                    alertSet.push(tempDict);
+                    tempDict = {};
+                    notAllowed = true;
+                }
+                else {
+                    if(final[x].classes[i].available == 0){
+                        //gree
+                    }
+                    else if(final[x].classes[i].available != 0 && clss.indexOf(final[x].classes[i].class) > -1) {
+                        tempDict["class"] = final[x].classes[i].class;
+                        tempDict["prereq"] = final[x].classes[i].pre;
+                        alertSet.push(tempDict);
+                        tempDict = {};
+                        notAllowed = true;
+                    }
+                }
+            }
+        }
 
-    var f={"x":final};
-    var finals = JSON.stringify(f);
 
-  $.post('../updateStudentRecords.php',f,(data, textStatus, jqXHR) => {console.log(data); setUser();},"text");
+        //if = 0 means the classes are not before taken or are not red do below
+        if(notAllowed != true) {
+            var code ="";
+            code+=semesterName;
+            if( document.getElementsByClassName('sem')[0].checked == true) {
+                code+="S";
+            }
+            else {
+                code+="F";
+            }
+            var last=[];
+            var di ={}
+            //get username
+            var querys = window.location.search.substring(1);
+            var n = querys.split("=").pop();
+            for(j in clss) {
+                di["name"]=clss[j];
+                di["ucid"]=n;
+                di["code"]=code;
+                di["major"]="Science1";
+                last.push(di);
+                di={};
+            }
+
+            var f={"x":last};
+            var finals = JSON.stringify(f);
+
+            $.post('../updateStudentRecords.php',f,(data, textStatus, jqXHR) => {console.log(data); setUser();},"text");
+        }
+        else {
+            let c = "";
+            for(i in alertSet) {
+                c += alertSet[i].class;
+                if(alertSet[i].prereq != "taken") {
+                    c += " Missing PreReq: ";
+                    for(x in alertSet[i].prereq) {
+                        c += alertSet[i].prereq[x];
+                        c += ", ";
+                    }
+                    c += "\n";
+                }
+                else {
+                    c += ": Already Taken";
+                    c += "\n";
+                }
+            }
+            alert(c);
+        }
+    },"text");
+
 }
 
 function editSemester() {
